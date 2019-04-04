5e3701b1f804c63ddc254c68aacc995dcbce96ea
timhot <timhot@hotmail.com>
2019-04-04 15:58:10 +1300

    Changed app name from Cacophonomter to Bird Monitor.
    Had to move classes to new package/folder
    
    Also accepted offer of upgrading Gradle - which may have bee the reason for the context variable in lots of the xml files to be 'corrupt' ie the part of the context string was repeated

---
+++ app/src/androidTest/java/nz/org/cacophony/birdmonitor/HelperCode.java
@@ -0,0 +1,117 @@
+package nz.org.cacophony.birdmonitor;
+
+import android.content.Context;
+import android.util.Log;
+import java.util.Date;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static nz.org.cacophony.cacophonometer.IdlingResourceForEspressoTesting.recordIdlingResource;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+class HelperCode {
+
+   public static void useTestServerAndShortRecordings(){
+
+       Context targetContext = getInstrumentation().getTargetContext();
+       Prefs prefs = new Prefs(targetContext);
+
+        if (!prefs.getUseTestServer()){
+            Util.setUseTestServer(targetContext, true);
+            prefs.setUseShortRecordings(true);
+        }
+    }
+
+    public static void signOutUser(Prefs prefs){
+
+        prefs.setUsername(null);
+        prefs.setUsernamePassword(null);
+        prefs.setUserSignedIn(false);
+    }
+
+    public static void dismissWelcomeDialog(){
+        try{
+
+            try {
+                while (!recordIdlingResource.isIdleNow()) {
+                    recordIdlingResource.decrement();
+                }
+            }catch (Exception ex){
+                Log.e("Record", ex.getLocalizedMessage());
+            }
+            onView(allOf(withId(android.R.id.button1), withText("OK"))).perform(scrollTo(),click());
+        }catch (Exception ex){
+            Log.e("HelperCode", ex.getLocalizedMessage());
+        }
+    }
+
+    public static void dismissDialogWithYes(){
+        try{
+            onView(allOf(withId(android.R.id.button1), withText("YES"))).perform(scrollTo(),click());
+        }catch (Exception ex){
+            Log.e("HelperCode", ex.getLocalizedMessage());
+        }
+    }
+
+    public static void signInUserTimhot(){
+
+        try {
+
+            Thread.sleep(1000); // had to put in sleep, as the GUI was replacing the username after I set it below
+
+            onView(withId(R.id.etUserNameOrEmailInput)).perform(replaceText("timhot"), closeSoftKeyboard());
+
+            onView(withId(R.id.etPasswordInput)).perform(replaceText("Pppother1"), closeSoftKeyboard());
+            onView(withId(R.id.btnSignIn)).perform(click());
+
+        }catch (Exception ex){
+            Log.e("SignInUser", ex.getLocalizedMessage());
+        }
+    }
+
+
+    public static void registerPhone(Prefs prefs){
+
+        try {
+            Thread.sleep(1000); // had to put in sleep, as could not work out how to consistently get groups to display before testing code tries to choose a group
+
+            onData(allOf(is(instanceOf(String.class)), is("tim1"))).perform(click());
+
+            // App automatically moves to Register Phone screen
+            // Now enter the device name
+
+            // Create a unique device name
+            Date now = new Date();
+
+            String deviceName = Long.toString(now.getTime()/1000);
+            prefs.setLastDeviceNameUsedForTesting(deviceName); // save the device name so can find recordings for it later
+
+            onView(withId(R.id.etDeviceNameInput)).perform(replaceText(deviceName), closeSoftKeyboard());
+            onView(withId(R.id.btnRegister)).perform(click());
+
+        }catch (Exception ex){
+            Log.e("RegisterPhone", ex.getLocalizedMessage());
+        }
+    }
+
+    public static void unRegisterPhone(){
+
+        try {
+            Thread.sleep(1000);
+            onView(withId(R.id.btnUnRegister)).perform(click());
+            Thread.sleep(1000);
+            dismissDialogWithYes();
+        }catch (Exception ex){
+            Log.e("RegisterPhone", ex.getLocalizedMessage());
+        }
+    }
+}
---
+++ app/src/main/java/nz/org/cacophony/birdmonitor/BootReceiver.java
@@ -0,0 +1,94 @@
+
+package nz.org.cacophony.birdmonitor;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.PowerManager;
+import android.util.Log;
+
+import static android.content.Context.POWER_SERVICE;
+
+
+/**
+ * BootReceiver runs when phone is restarted. It does the job that the MainActivity would have done of setting up the repeating alarm.
+ * This means the recordings will be made without having to reopen the application.
+ * If the user needs to change any settings, then they will need to open the application which will save those settings
+ * in the apps shared preferences (file on phone) that are accessed via the Prefs class.
+ */
+public class BootReceiver extends BroadcastReceiver {
+
+
+    private static final String TAG = BootReceiver.class.getName();
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+
+        if (intent.getAction() == null) {
+            Log.e(TAG, "intent.getAction() is null");
+            return;
+        }
+
+        if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            return;
+        }
+
+        try {
+
+
+            final Thread thread = new Thread() {
+                @Override
+                public void run() {
+
+                    PowerManager powerManager = (PowerManager) context.getSystemService(POWER_SERVICE);
+                    if (powerManager == null) {
+                        Log.e(TAG, "powerManager is null");
+                        return;
+                    }
+
+                    final PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                            "Cacophonometer:BootReceiverWakelockTag");
+                    long timeout = 1000 * 60 * 2;  // give the boot stuff two minutes to run - but the enable flight mode does not seem to be working (however long I wait).
+                    wakeLock.acquire(timeout); // finally never seems to run which is why I used a timeout on the wakelock creation
+                    try {
+
+
+                        ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+                        toneGen1.startTone(ToneGenerator.TONE_CDMA_CALL_SIGNAL_ISDN_NORMAL, 1000);
+
+                        Util.createTheNextSingleStandardAlarm(context);
+
+                        DawnDuskAlarms.configureDawnAndDuskAlarms(context, true);
+
+                        Util.createCreateAlarms(context);
+
+                        Util.setUpLocationUpdateAlarm(context);
+
+                        Util.enableFlightMode(context);
+
+                    } catch (Exception e) {
+                        Log.e(TAG, "Error disabling flight mode");
+                    } finally {
+                        wakeLock.release();
+                    }
+                }
+            };
+            thread.start();
+
+        } catch (Exception ex) {
+            ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+            toneGen1.startTone(ToneGenerator.TONE_CDMA_ABBR_ALERT, 5000);
+            try {
+                Thread.sleep(3000);
+            } catch (Exception e) {
+                Log.e(TAG, ex.getLocalizedMessage());
+            }
+        }
+
+
+    }
+
+}
