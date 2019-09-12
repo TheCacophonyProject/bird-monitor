5a79608cb2eeeecd7caf67b1b45ae9846a18692b
Giampaolo <g.ferraro22@gmail.com>
2019-09-13 11:58:48 +1200

    add auto updating service

--- app/src/main/java/nz/org/cacophony/birdmonitor/views/RootedFragment.java
+++ app/src/main/java/nz/org/cacophony/birdmonitor/views/RootedFragment.java
@@ -1,7 +1,17 @@
 package nz.org.cacophony.birdmonitor.views;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+>>>>>>> Stashed changes
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,14 +22,67 @@ import nz.org.cacophony.birdmonitor.Prefs;
 import nz.org.cacophony.birdmonitor.R;
 import nz.org.cacophony.birdmonitor.Util;
 
+import org.fdroid.fdroid.privileged.IPrivilegedCallback;
+import org.fdroid.fdroid.privileged.IPrivilegedService;
+
+import java.net.URI;
+
 public class RootedFragment extends Fragment {
-
+//
+     private IPrivilegedService service;
+//    private RemoteServiceConnection serviceConnection;
     private static final String TAG = "RootedFragment";
-
+    private static final String PRIVILEGED_EXTENSION_SERVICE_INTENT
+            = "org.fdroid.fdroid.privileged.IPrivilegedService";
+    public static final int ACTION_INSTALL_REPLACE_EXISTING = 2;
 
     private Switch swRooted;
     private Button btnFinished;
     private TextView tvMessages;
+
+    protected void installPackageInternal(Context context) {
+        Log.e("install","install packaged internal");
+        ServiceConnection mServiceConnection = new ServiceConnection() {
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                Log.e("Install","Connected");
+                IPrivilegedService privService = IPrivilegedService.Stub.asInterface(service);
+
+                IPrivilegedCallback callback = new IPrivilegedCallback.Stub() {
+                    @Override
+                    public void handleResult(String packageName, int returnCode) throws RemoteException {
+                      Log.d("install", "return ccode " + returnCode);
+                    }
+                };
+
+                try {
+                    boolean hasPermissions = privService.hasPrivilegedPermissions();
+                    Log.e("Install","permissions?" + hasPermissions);
+
+                    if (!hasPermissions) {
+                        return;
+                    }
+
+                    privService.installPackage(Uri.parse("http://www.google.com"), ACTION_INSTALL_REPLACE_EXISTING,
+                            null, callback);
+                } catch (RemoteException e) {
+                    Log.e(TAG, "RemoteException", e);
+
+                }catch(Exception e){
+                    Log.e(TAG, "RemoteException", e);
+
+                }
+            }
+
+            public void onServiceDisconnected(ComponentName name) {
+            }
+        };
+
+        Intent serviceIntent = new Intent(PRIVILEGED_EXTENSION_SERVICE_INTENT);
+        serviceIntent.setPackage("org.fdroid.fdroid.privileged");
+        context.getApplicationContext().bindService(serviceIntent, mServiceConnection,
+                Context.BIND_AUTO_CREATE);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -43,7 +106,7 @@ public class RootedFragment extends Fragment {
                 Util.checkSuperUserAccess();
             }
         });
-
+        installPackageInternal(this.getContext());
         return view;
     }
 
