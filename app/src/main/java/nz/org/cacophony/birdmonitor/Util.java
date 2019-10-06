676e764de2f9cb3209d06c33275e2b0ffcb08b2a
GP <g.ferraro22@gmail.com>
2019-10-07 09:52:19 +1300

    Fix issues with only sunrise alarms scheduled (#92)
    * added missing breaks

--- app/src/main/java/nz/org/cacophony/birdmonitor/Util.java
+++ app/src/main/java/nz/org/cacophony/birdmonitor/Util.java
@@ -895,9 +895,11 @@ public class Util {
                     case Prefs.SUNRISE_OFFSET:
                         wakeUpTime = getNoon(context, calNow).getTimeInMillis() + prefs.getSunriseOffsetMillis();
                         alarm = new Alarm(wakeUpTime, Prefs.NOON_OFFSET);
+                        break;
                     case Prefs.NOON_OFFSET:
                         wakeUpTime = getSunset(context, calNow).getTimeInMillis() + prefs.getSunsetOffsetMillis();
                         alarm = new Alarm(wakeUpTime, Prefs.SUNSET_OFFSET);
+                        break;
                     case Prefs.SUNSET_OFFSET:
                         calNow.add(Calendar.DAY_OF_YEAR, 1);
                         wakeUpTime = getSunrise(context, calNow).getTimeInMillis() + prefs.getSunriseOffsetMillis();
@@ -950,7 +952,6 @@ public class Util {
         Prefs prefs = new Prefs(context);
         Alarm nextAlarm = getNextAlarm(context, prefs, relativeTo);
         Intent myIntent = getRepeatingAlarmIntent(context, nextAlarm.OffsetType);
-
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
         if (alarmManager == null) {
             Log.e(TAG, "alarmManager is null");
